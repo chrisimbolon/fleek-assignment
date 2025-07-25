b2726e2c1073b3a1f81bebd134743db2932db321
Christyan Simbolon <sambungdata@gmail.com>
2025-07-25 17:14:25 +0700

    feat(api): implement POST /generate GET and /status/{job_id}  endpoint with job creation and validation

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -3,4 +3,19 @@ from pydantic import BaseModel
 import uuid
 from backend.tasks.worker import generate_image_task
 
+router = APIRouter
 
+class GenerateRequest(BaseModel):
+    prompt: str
+    parameters : dict
+
+@router.post("/generate")
+async def generate(request : GenerateRequest):
+    job_id = str(uuid.uuid4())
+    generate_image_task.delay(job_id, request.prompt, request.parameters)
+    return {"job_id": job_id}
+
+@router.get("/status/{job_id}")
+async def get_status(job_id:str):
+    # TODO: Rejplace with real DB call 
+    return {"job_id": job_id, "status": "pending", "result": None}
